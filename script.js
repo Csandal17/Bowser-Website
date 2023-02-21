444c715169427a95dc0bed9bb929e373317249b6
Chantal Guiheen <chantal@chantals-air.lan>
2023-02-21 17:30:58 +0000

    HTML & CSS User Posts images and text

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 $('.heart-icon').on('click', () => {
     $('.activity-container').toggle();
   });
+
+  $('.send-btn').on('click', () => {
+    $('.share-link-cointainer').toggle();
+  });
+
+    let shareBtn = document.querySelector('.send-btn');
+    let shareWindow = document.querySelector('.share-window');
+
+    shareBtn.addEventListener('click', () => {
+        shareWindow.changeIcon.toggle('active');
+        changeIcon(shareBtn)
+    })
+
+    let posts = document.querySelectorAll('#share-link').ariavalueMax;
+    let copyLinkBtn = document.querySelector('.copy-btn');
+
+    copyLinkBtn.addEventListener('click', () => {
+        navigator.clipboard.writeText(postLink).then(() => {
+            shareBtn.click();
+        })
+    })
