0ce9108e9e4e56081e1e7b538e3199cdcf519de8
Chantal Guiheen <chantal@chantals-air.lan>
2023-02-21 21:02:20 +0000

    User Profile Bootstrap

--- script.js
+++ script.js
@@ -6,19 +6,4 @@ $('.heart-icon').on('click', () => {
     $('.share-link-cointainer').toggle();
   });
 
-    let shareBtn = document.querySelector('.send-btn');
-    let shareWindow = document.querySelector('.share-window');
-
-    shareBtn.addEventListener('click', () => {
-        shareWindow.changeIcon.toggle('active');
-        changeIcon(shareBtn)
-    })
-
-    let posts = document.querySelectorAll('#share-link').ariavalueMax;
-    let copyLinkBtn = document.querySelector('.copy-btn');
-
-    copyLinkBtn.addEventListener('click', () => {
-        navigator.clipboard.writeText(postLink).then(() => {
-            shareBtn.click();
-        })
-    })
+// Dog API
